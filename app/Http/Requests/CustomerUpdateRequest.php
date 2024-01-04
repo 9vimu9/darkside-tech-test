e2bfa72a2b824172233a2c8f83f041c223d3cd2d
vim <9wimu9@gmail.com>
2024-01-04 19:18:17 +0000

    Created Request classes for Customer store and update

---
+++ app/Http/Requests/CustomerUpdateRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CustomerUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id'=>['exists:customers,id'],
+            'name' =>['required','max:255','min:10'],
+            'email' => ['required','unique:customers,email,'. $this->input('id') .'ID','email','max:255'],
+            'address' =>['required','max:255','min:10'],
+            'telephone_number' => ['required','max:25','min:7'],
+        ];
+    }
+}
